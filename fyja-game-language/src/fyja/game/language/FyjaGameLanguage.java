0619a1596e496bf6f58b317d7f31f837c9fcebc9
Jean Carlos P. Bairro <jeancasulo@gmail.com>
2016-10-13 13:20:38 -0300

    Removidos comentários desnecessários

--- fyja-game-language/src/fyja/game/language/FyjaGameLanguage.java
+++ fyja-game-language/src/fyja/game/language/FyjaGameLanguage.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fyja.game.language;
 
 import interpretador.Comando;
@@ -10,7 +5,6 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Image;
 import interpretador.Interpretador;
-import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
